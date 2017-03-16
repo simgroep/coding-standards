14b0a9df3b368c733ab0f8c3ca00ead5da44b174
Sander van Thillo <othillo@othillo.nl>
2017-03-16 15:28:30 +0100

    use friendsofphp/php-cs-fixer 2.1

---
+++ examples/ordered_imports.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use RuntimeException;
+use LogicException;
+
+class MyRuntimeException extends RuntimeException
+{
+}
+
+class MyLogicException extends LogicException
+{
+}
---
+++ examples/phpdoc_order.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @return bool
+ * @throws RuntimeException
+ * @param string $foo
+ * @param int    $bar
+ */
+function my_function(string $foo, int $bar)
+{
+    return false;
+}
