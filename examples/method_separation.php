00c3042c5091b31e8e035d7510f2934067f5fa6e
Saif Bechan <s.bechan@simgroep.nl>
2017-08-01 13:27:19 +0200

    Added method_separation

---
+++ examples/method_separation.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace examples;
+
+class MyClass
+{
+    public function my_first_method()
+    {
+        return 'foo';
+    }
+    public function my_second_method()
+    {
+        return 'bar';
+    }
+}
